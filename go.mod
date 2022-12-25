da98d39cdd3e51bdbf918738f64cf1432d7ba32d
rabobahago <rabobahago@gmail.com>
2022-12-25 10:37:02 +0100

    booking app starter code added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module book-app
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var conferenceName = "Go Conference"
+    const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to our",conferenceName,"booking application")
+	fmt.Println("Get your tickets here to attend")
+
+   fmt.Println(remainingTickets)
+}
