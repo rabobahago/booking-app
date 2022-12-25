818f3b346c2351df88e34e1c6db409a75dc3b3fb
rabobahago <rabobahago@gmail.com>
2022-12-25 11:38:17 +0100

    user data logic added

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main(){
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
     const conferenceTickets = 50
 	var remainingTickets = 50
-	fmt.Println("Welcome to our",conferenceName,"booking application")
-	fmt.Println("Get your tickets here to attend")
 
-   fmt.Println(remainingTickets)
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceName, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+    var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+    fmt.Printf("Please Enter your first name: ")
+    fmt.Scan(&firstName)
+	fmt.Printf("Please Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Printf("Please Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Printf("Please Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+    fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v", firstName, lastName, userTickets, email)
 }
